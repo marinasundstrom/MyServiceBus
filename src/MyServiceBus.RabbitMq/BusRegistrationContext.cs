a4fc1d598d7635ef817ec11aedc1842ea0ebe00f
Marina Sundström <marna.li@outlook.com>
2025-04-07 22:31:52 +0200

    Move Mediator into MyServiceBus project

--- src/MyServiceBus.RabbitMq/BusRegistrationContext.cs
+++ src/MyServiceBus.RabbitMq/BusRegistrationContext.cs
@@ -11,4 +11,22 @@ internal sealed class BusRegistrationContext : IBusRegistrationContext
     }
 
     public IServiceProvider ServiceProvider { get; }
+
+    public void ConfigureEndpoints<T>(IReceiveConfigurator<T> configurator, IEndpointNameFormatter endpointNameFormatter = null)
+            where T : IReceiveEndpointConfigurator
+    {
+
+    }
+}
+
+public interface IReceiveEndpointConfigurator
+{
+}
+
+public interface IEndpointNameFormatter
+{
+}
+
+public interface IReceiveConfigurator<T>
+{
 }
