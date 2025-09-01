b686d08e41de3c1a33b284d1fea24583e9d5f1c7
Marina Sundström <marna.li@outlook.com>
2025-09-02 01:00:56 +0200

    feat(java): add request client

--- src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/RequestClient.java
+++ src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/RequestClient.java
@@ -5,5 +5,6 @@ import java.util.concurrent.CompletableFuture;
 import com.myservicebus.tasks.CancellationToken;
 
 public interface RequestClient<TRequest> {
-    <TResponse> CompletableFuture<TResponse> getResponse(TRequest request, CancellationToken cancellationToken);
+    <TResponse> CompletableFuture<TResponse> getResponse(TRequest request, Class<TResponse> responseType,
+            CancellationToken cancellationToken);
 }
