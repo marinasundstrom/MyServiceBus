87e4ba61bd35cf6c1d22805f5dba4b723c40843c
Marina Sundström <marna.li@outlook.com>
2025-09-02 01:54:35 +0200

    Update testapp

--- src/Java/testapp/src/main/java/com/myservicebus/testapp/Main.java
+++ src/Java/testapp/src/main/java/com/myservicebus/testapp/Main.java
@@ -4,39 +4,35 @@ import java.util.UUID;
 
 import com.myservicebus.MyService;
 import com.myservicebus.MyServiceImpl;
-import com.myservicebus.ServiceBus;
 import com.myservicebus.di.ServiceCollection;
-import com.myservicebus.rabbitmq.RabbitMqTransport;
+import com.myservicebus.rabbitmq.RabbitMqBus;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         ServiceCollection services = new ServiceCollection();
         services.addScoped(MyService.class, MyServiceImpl.class);
 
-        var serviceBus = ServiceBus.configure(services, x -> {
-            x.addConsumer(SubmitOrderConsumer.class);
+        var serviceBus = RabbitMqBus.configure(services, cfg -> {
+            cfg.addConsumer(SubmitOrderConsumer.class);
 
-            RabbitMqTransport.configure(x, (context, cfg) -> {
-
-                cfg.host("localhost", h -> {
-                    h.username("guest");
-                    h.password("guest");
-                });
-
-                /*
-                 * cfg.message(SubmitOrder.class, m -> {
-                 * m.setEntityName("TestApp.SubmitOrder");
-                 * });
-                 * 
-                 * cfg.message(OrderSubmitted.class, m -> {
-                 * m.setEntityName("TestApp.OrderSubmitted");
-                 * });
-                 * 
-                 * cfg.receiveEndpoint("submit-order-consumer", e -> {
-                 * e.configureConsumer(context, SubmitOrderConsumer.class);
-                 * });
-                 */
+            cfg.host("localhost", h -> {
+                h.username("guest");
+                h.password("guest");
             });
+
+            /*
+             * cfg.message(SubmitOrder.class, m -> {
+             * m.setEntityName("TestApp.SubmitOrder");
+             * });
+             * 
+             * cfg.message(OrderSubmitted.class, m -> {
+             * m.setEntityName("TestApp.OrderSubmitted");
+             * });
+             * 
+             * cfg.receiveEndpoint("submit-order-consumer", e -> {
+             * e.configureConsumer(context, SubmitOrderConsumer.class);
+             * });
+             */
         });
 
         serviceBus.start();
