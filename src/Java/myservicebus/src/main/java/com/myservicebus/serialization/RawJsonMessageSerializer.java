33ede32d5af16a350e0fc69d6a5fc33083c93ccb
Marina Sundström <marna.li@outlook.com>
2025-09-03 22:44:03 +0200

    Align Java serialization API with C# (#111)

--- src/Java/myservicebus/src/main/java/com/myservicebus/serialization/RawJsonMessageSerializer.java
+++ src/Java/myservicebus/src/main/java/com/myservicebus/serialization/RawJsonMessageSerializer.java
@@ -1,7 +1,7 @@
 package com.myservicebus.serialization;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.myservicebus.SendContext;
+import java.io.IOException;
 
 public class RawJsonMessageSerializer implements MessageSerializer {
     private final ObjectMapper mapper;
@@ -12,7 +12,7 @@ public class RawJsonMessageSerializer implements MessageSerializer {
     }
 
     @Override
-    public byte[] serialize(SendContext context) throws Exception {
+    public <T> byte[] serialize(MessageSerializationContext<T> context) throws IOException {
         context.getHeaders().put("content_type", "application/json");
         return mapper.writeValueAsBytes(context.getMessage());
     }
