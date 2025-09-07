d9ce131a1cb4a80a331b49b925dc1e944b9bf77d
Marina Sundström <marna.li@outlook.com>
2025-09-07 08:49:54 +0200

    feat: add getRequiredService to Java DI (#194)

--- src/Java/myservicebus-di/src/main/java/com/myservicebus/di/ServiceProvider.java
+++ src/Java/myservicebus-di/src/main/java/com/myservicebus/di/ServiceProvider.java
@@ -5,6 +5,14 @@ import java.util.Set;
 public interface ServiceProvider {
     <T> T getService(Class<T> type);
 
+    default <T> T getRequiredService(Class<T> type) {
+        T service = getService(type);
+        if (service == null) {
+            throw new IllegalStateException("Required service of type " + type.getName() + " is not registered.");
+        }
+        return service;
+    }
+
     <T> Set<T> getServices(Class<T> iface);
 
     ServiceScope createScope();
