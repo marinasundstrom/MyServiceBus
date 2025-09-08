d71037448e067a466d7e15357945809bc19402c5
Marina Sundström <marna.li@outlook.com>
2025-09-08 10:31:49 +0200

    Add anonymous message support for Java (#224)

--- src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/PublishEndpoint.java
+++ src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/PublishEndpoint.java
@@ -8,6 +8,11 @@ import com.myservicebus.tasks.CancellationToken;
 public interface PublishEndpoint {
     <T> CompletableFuture<Void> publish(T message, CancellationToken cancellationToken);
 
+    default <T> CompletableFuture<Void> publish(Class<T> messageType, Object message,
+            CancellationToken cancellationToken) {
+        return publish(MessageProxy.create(messageType, message), cancellationToken);
+    }
+
     default CompletableFuture<Void> publish(PublishContext context) {
         return publish(context.getMessage(), context.getCancellationToken());
     }
@@ -24,5 +29,20 @@ public interface PublishEndpoint {
 
     default <T> CompletableFuture<Void> publish(T message) {
         return publish(message, CancellationToken.none);
+    }
+
+    default <T> CompletableFuture<Void> publish(Class<T> messageType, Object message,
+            Consumer<PublishContext> contextCallback, CancellationToken cancellationToken) {
+        T proxy = MessageProxy.create(messageType, message);
+        return publish(proxy, contextCallback, cancellationToken);
+    }
+
+    default <T> CompletableFuture<Void> publish(Class<T> messageType, Object message,
+            Consumer<PublishContext> contextCallback) {
+        return publish(messageType, message, contextCallback, CancellationToken.none);
+    }
+
+    default <T> CompletableFuture<Void> publish(Class<T> messageType, Object message) {
+        return publish(messageType, message, CancellationToken.none);
     }
 }
--- src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/SendEndpoint.java
+++ src/Java/myservicebus-abstractions/src/main/java/com/myservicebus/SendEndpoint.java
@@ -8,6 +8,11 @@ import com.myservicebus.tasks.CancellationToken;
 public interface SendEndpoint {
     <T> CompletableFuture<Void> send(T message, CancellationToken cancellationToken);
 
+    default <T> CompletableFuture<Void> send(Class<T> messageType, Object message,
+            CancellationToken cancellationToken) {
+        return send(MessageProxy.create(messageType, message), cancellationToken);
+    }
+
     default CompletableFuture<Void> send(SendContext context) {
         return send(context.getMessage(), context.getCancellationToken());
     }
@@ -24,5 +29,20 @@ public interface SendEndpoint {
 
     default <T> CompletableFuture<Void> send(T message) {
         return send(message, CancellationToken.none);
+    }
+
+    default <T> CompletableFuture<Void> send(Class<T> messageType, Object message,
+            Consumer<SendContext> contextCallback, CancellationToken cancellationToken) {
+        T proxy = MessageProxy.create(messageType, message);
+        return send(proxy, contextCallback, cancellationToken);
+    }
+
+    default <T> CompletableFuture<Void> send(Class<T> messageType, Object message,
+            Consumer<SendContext> contextCallback) {
+        return send(messageType, message, contextCallback, CancellationToken.none);
+    }
+
+    default <T> CompletableFuture<Void> send(Class<T> messageType, Object message) {
+        return send(messageType, message, CancellationToken.none);
     }
 }
