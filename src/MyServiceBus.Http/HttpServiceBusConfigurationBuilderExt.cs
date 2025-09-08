37ff2c9ec82dd2117ae2abddbff7a44a7f59e2f3
Marina Sundström <marna.li@outlook.com>
2025-09-09 00:11:43 +0200

    Add HTTP endpoint configuration extensions (#246)

--- src/MyServiceBus.Http/HttpServiceBusConfigurationBuilderExt.cs
+++ src/MyServiceBus.Http/HttpServiceBusConfigurationBuilderExt.cs
@@ -1,5 +1,6 @@
 using MyServiceBus.Serialization;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace MyServiceBus;
 
@@ -7,6 +8,28 @@ public static class HttpServiceBusConfigurationBuilderExt
 {
     public static IBusRegistrationConfigurator UsingHttp(this IBusRegistrationConfigurator builder, Uri baseAddress)
     {
+        builder.Services.AddSingleton<ITransportFactory, HttpTransportFactory>();
+        builder.Services.AddSingleton<IMessageBus>(sp => new MessageBus(
+            sp.GetRequiredService<ITransportFactory>(),
+            sp,
+            sp.GetRequiredService<ISendPipe>(),
+            sp.GetRequiredService<IPublishPipe>(),
+            sp.GetRequiredService<IMessageSerializer>(),
+            baseAddress,
+            sp.GetRequiredService<ISendContextFactory>(),
+            sp.GetRequiredService<IPublishContextFactory>()));
+        return builder;
+    }
+
+    public static IBusRegistrationConfigurator UsingHttp(
+        this IBusRegistrationConfigurator builder,
+        Uri baseAddress,
+        Action<IBusRegistrationContext, IHttpFactoryConfigurator> configure)
+    {
+        var httpConfigurator = new HttpFactoryConfigurator(baseAddress);
+
+        builder.Services.AddSingleton<IHttpFactoryConfigurator>(httpConfigurator);
+        builder.Services.AddSingleton<IPostBuildAction>(new PostBuildHttpConfigureAction(configure, httpConfigurator));
         builder.Services.AddSingleton<ITransportFactory, HttpTransportFactory>();
         builder.Services.AddSingleton<IMessageBus>(sp => new MessageBus(
             sp.GetRequiredService<ITransportFactory>(),
