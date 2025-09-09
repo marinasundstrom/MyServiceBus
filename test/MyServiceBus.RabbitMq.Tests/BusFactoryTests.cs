07e8448add8c5d39a6aeba7062145bfe8507e81f
Marina Sundström <marna.li@outlook.com>
2025-09-09 15:29:36 +0200

    Move test

--- test/MyServiceBus.RabbitMq.Tests/BusFactoryTests.cs
+++ test/MyServiceBus.RabbitMq.Tests/BusFactoryTests.cs
@@ -1,12 +1,14 @@
 using MyServiceBus;
 using Xunit;
+using Xunit.Sdk;
 
 public class BusFactoryTests
 {
     [Fact]
+    [Throws(typeof(NotNullException))]
     public void Factory_creates_bus()
     {
-        var bus = MessageBus.Factory.CreateUsingRabbitMq(cfg => cfg.Host("localhost"));
+        var bus = MyServiceBus.MessageBus.Factory.CreateUsingRabbitMq(cfg => cfg.Host("localhost"));
         Assert.NotNull(bus);
     }
 }
