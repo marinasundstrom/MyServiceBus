2f0101f805d06b59adcd09fff4633f686e91c047
Marina Sundström <marna.li@outlook.com>
2025-09-11 12:29:36 +0200

    Add cancellable scheduling for Java

--- src/Java/myservicebus/src/main/java/com/myservicebus/MessageSchedulerImpl.java
+++ src/Java/myservicebus/src/main/java/com/myservicebus/MessageSchedulerImpl.java
@@ -1,29 +1,50 @@
 package com.myservicebus;
 
 import java.time.Instant;
-import java.util.concurrent.CompletableFuture;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
+import com.myservicebus.tasks.CancellationToken;
 
 public class MessageSchedulerImpl implements MessageScheduler {
     private final PublishEndpoint publishEndpoint;
     private final SendEndpointProvider sendEndpointProvider;
     private final JobScheduler jobScheduler;
 
-    public MessageSchedulerImpl(PublishEndpoint publishEndpoint, SendEndpointProvider sendEndpointProvider, JobScheduler jobScheduler) {
+    public MessageSchedulerImpl(PublishEndpoint publishEndpoint,
+            SendEndpointProvider sendEndpointProvider,
+            JobScheduler jobScheduler) {
         this.publishEndpoint = publishEndpoint;
         this.sendEndpointProvider = sendEndpointProvider;
         this.jobScheduler = jobScheduler;
     }
 
     @Override
-    public <T> CompletableFuture<Void> schedulePublish(T message, Instant scheduledTime) {
-        return jobScheduler.schedule(scheduledTime, () -> publishEndpoint.publish(message));
+    public <T> CompletionStage<ScheduledMessageHandle> schedulePublish(T message,
+            Instant scheduledTime,
+            CancellationToken cancellationToken) {
+        return jobScheduler.schedule(scheduledTime, token -> publishEndpoint.publish(message, token), cancellationToken)
+                .thenApply(ScheduledMessageHandle::new);
     }
 
     @Override
-    public <T> CompletableFuture<Void> scheduleSend(String destination, T message, Instant scheduledTime) {
-        return jobScheduler.schedule(scheduledTime, () -> {
+    public <T> CompletionStage<ScheduledMessageHandle> scheduleSend(String destination,
+            T message,
+            Instant scheduledTime,
+            CancellationToken cancellationToken) {
+        return jobScheduler.schedule(scheduledTime, token -> {
             SendEndpoint endpoint = sendEndpointProvider.getSendEndpoint(destination);
-            return endpoint.send(message);
-        });
+            return endpoint.send(message, token);
+        }, cancellationToken).thenApply(ScheduledMessageHandle::new);
+    }
+
+    @Override
+    public CompletionStage<Void> cancelScheduledPublish(UUID tokenId, CancellationToken cancellationToken) {
+        return jobScheduler.cancel(tokenId);
+    }
+
+    @Override
+    public CompletionStage<Void> cancelScheduledSend(UUID tokenId, CancellationToken cancellationToken) {
+        return jobScheduler.cancel(tokenId);
     }
 }
