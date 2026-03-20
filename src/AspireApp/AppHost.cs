274de8f2c1b31f5b82de0a929c71542efd469b5e
Marina Sundström <marna.li@outlook.com>
2026-03-20 23:25:43 +0100

    Configure RabbitMQ in Aspire

--- src/AspireApp/AppHost.cs
+++ src/AspireApp/AppHost.cs
@@ -2,13 +2,17 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var rabbitmq = builder.AddRabbitMQ("messaging")
-//    .WithDataVolume(isReadOnly: false);
+var rabbitUser = builder.AddParameter("rabbitmq-user", "guest", secret: true);
+var rabbitPassword = builder.AddParameter("rabbitmq-password", "guest", secret: true);
+
+var rabbitmq = builder.AddRabbitMQ("messaging", rabbitUser, rabbitPassword, port: 5672)
+    .WithManagementPlugin(port: 15672)
+    .WithDataVolume(isReadOnly: false);
 
 var csharpTestApp = builder.AddProject<TestApp>("testapp")
-    //.WithReference(rabbitmq)
-    .WithExternalHttpEndpoints();
-//.WaitFor(rabbitmq);
+    .WithReference(rabbitmq)
+    .WithExternalHttpEndpoints()
+    .WaitFor(rabbitmq);
 
 var javaTestApp = builder.AddJavaApp(
     "testapp-java",
@@ -19,8 +23,8 @@ var javaTestApp = builder.AddJavaApp(
         OtelAgentPath = "../../AspireApp/agents",
     })
     .WithEnvironment("OTEL_EXPORTER_OTLP_CERTIFICATE", "../../AspireApp/agents/aspire-localhost-cert.pem")
-    //.WithReference(rabbitmq)
-    .WithExternalHttpEndpoints();
-//.WaitFor(rabbitmq);
+    .WithReference(rabbitmq)
+    .WithExternalHttpEndpoints()
+    .WaitFor(rabbitmq);
 
 builder.Build().Run();
